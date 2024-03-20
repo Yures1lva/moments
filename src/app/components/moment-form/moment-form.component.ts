d24920c5dc96928f1a85ebaad589a1ce61ddfaec
Yures1lva <yure.siilva9@gmail.com>
2024-03-20 20:20:30 -0300

    começando validação de form

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
+
 export class MomentFormComponent implements OnInit {
  @Input() btnText!: string
   
@@ -13,10 +14,29 @@ export class MomentFormComponent implements OnInit {
 
 
  ngOnInit(): void {
-   
+   this.momentForm = new FormGroup({
+    id: new FormControl(''),
+    title: new FormControl('', [Validators.required]),
+    descipt: new FormControl('', [Validators.required]),
+    image: new FormControl(''),
+
+   })
  }
 
+ get title(){
+  return this.momentForm.get('title')!
+ }
+
+ get description(){
+  return this.momentForm.get('description')!
+ }
+
+
  submit(){
+
+  if(this.momentForm.invalid ){
+    return
+  }
   console.log("form enviado")
  }
 
